33158ed040f82c9deeef162443048dd98935f794
Lennert Van Geert <lennert.vangeert@gmail.com>
2025-09-28 16:44:30 +0200

    Remove translation hooks from NotFoundPage for static content display

--- src/modules/misc/notFoundPage/index.tsx
+++ src/modules/misc/notFoundPage/index.tsx
@@ -1,14 +1,12 @@
 import Head from "@global/head";
 import { Center } from "@mantine/core";
-import { useTranslation } from "react-i18next";
 
 const NotFoundPage = () => {
-  const { t } = useTranslation();
   return (
     <>
-    <Head title={t('404 - Not Found')} description={t('This page was not found')} />
+      <Head title="404 - Not Found" description="This page was not found" />
       <Center h="100vh">
-        <h1>{t('404 - Not Found')}</h1>
+        <h1>404 - Not Found</h1>
       </Center>
     </>
   );
